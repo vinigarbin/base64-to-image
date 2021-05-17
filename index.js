6759e730945a3d4223906245b5792833b5132c4e
Vinicius Garbin <vinigarbin@gmail.com>
2021-05-17 18:55:12 -0300

    adjusted name function

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const db = require("./connection");
 (async () => {
     const images = await db.selectImages();
     images?.map(img => {
-        blobToFile(img.blob, img.file_name)
+        toFile(img.blob, img.file_name)
     })
     return;
 })();
 
-async function blobToFile(base64, fileName) {
+async function toFile(base64, fileName) {
     var optionalObj = { 'fileName': fileName, 'type': process.env.type || 'png' };
     base64ToImage(base64, process.env.dir, optionalObj);
 }
